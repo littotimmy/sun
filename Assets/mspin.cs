8da541ba95a926d2f575d8f2f34e229e97e613bf
littotimmy <littotimmy@gmail.com>
2018-04-11 15:32:04 +0530

    working version

---
+++ Assets/mspin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class mspin : MonoBehaviour
+{
+    public GameObject earth;
+    public float speed;
+    public float speed1;
+    
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        //transform.position = new Vector3(earth.transform.position.x + (Mathf.Sin(50 * Mathf.PI/ 180)), earth.transform.position.y, earth.transform.position.z + (Mathf.Cos(50 * Mathf.PI / 180)));
+        transform.RotateAround(earth.transform.position ,earth.transform.up, speed1 * Time.deltaTime);
+    }
+}
+
---
+++ Assets/spin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class spin : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject Sun;
+    public float speed2;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        transform.RotateAround(Sun.transform.position, Sun.transform.up, speed2 * Time.deltaTime);
+    }
+}
